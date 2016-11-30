5959e82f392477cd3b4c5a8bf1517fde1543382a
samuel santhosh <samuels410@gmail.com>
2016-11-30 09:33:51 +0530

    override css allow without checking csrf

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -108,6 +108,7 @@ require 'securerandom'
 #     }
 #
 class FilesController < ApplicationController
+  protect_from_forgery except: :show
   before_filter :require_user, only: :create_pending
   before_filter :require_context, except: [
     :assessment_question_show, :image_thumbnail, :show_thumbnail, :preflight,
